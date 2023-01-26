19ed66f253882eba391069b5da5603abaf4f144a
SoftDolphin <hawrang17@naver.com>
2023-01-26 17:44:47 +0900

    AWS 3일차

--- src/main/java/org/mega/book/springboot/service/PostsService.java
+++ src/main/java/org/mega/book/springboot/service/PostsService.java
@@ -3,11 +3,15 @@ package org.mega.book.springboot.service;
 import org.mega.book.springboot.domain.posts.Posts;
 import org.mega.book.springboot.domain.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
+import org.mega.book.springboot.web.dto.PostsListResponseDto;
 import org.mega.book.springboot.web.dto.PostsResponseDto;
 import org.mega.book.springboot.web.dto.PostsSaveRequestDto;
 import org.mega.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
